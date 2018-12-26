0ab89951475d869cc864d3025b5d8ac9afb5350b
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-26 16:54:41 -0600

    destroyed site and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'site/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'site#index'
+  root 'list#index'
 end
