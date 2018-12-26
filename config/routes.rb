0633d2d2442485bfe665e0d33e42a88e0ed4663e
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-26 17:15:44 -0600

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'site/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'list#index'
+  root 'lists#index'
 end
