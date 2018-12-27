2ed69330db892bda5597a1f40bf2dd3bdcaf45f8
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-27 13:26:51 -0600

    built out adding items to list feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def create
+    @list = List.find(params[:list_id])
+    @item = @list.items.build(item_params)
+    @item.save 
+
+    redirect_to list_path(@list)
   end
+
+  private 
+    def item_params
+      params.require(:item).permit(:description)
+    end
+
 end
