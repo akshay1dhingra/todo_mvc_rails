33bc2c9dacb358924f3e519d86735a276f2bff5b
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-26 18:34:54 -0600

    created create method in controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,4 +10,9 @@ class ListsController < ApplicationController
         @list = List.find(params[:id])
     end 
 
+    def create 
+        raise params.inspect
+        # @list = List.create(name: params[:name])
+    end 
+
 end
