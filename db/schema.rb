037460fed9277e3413109c548c2e606736a2c21c
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-26 20:47:44 -0600

    created my associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_224952) do
+ActiveRecord::Schema.define(version: 2018_12_27_024510) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "description"
+    t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
