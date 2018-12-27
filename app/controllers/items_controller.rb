d316ddc80e27f9790160298d68a3f7eccf24b3d4
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-27 15:44:42 -0600

    created if/else for when errors are present for item description

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,11 @@ class ItemsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @item = @list.items.build(item_params)
-    @item.save 
-
-    redirect_to list_path(@list)
+    if @item.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show"
+    end
   end
 
   private 
