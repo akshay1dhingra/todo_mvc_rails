2717d42ef6dcf61746619fa0205f2dd9ca1b2d98
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-28 12:29:14 -0600

    added complete/incomplete logic to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
     belongs_to :list
     validates :description, :presence => true
+
+    STATUS = {
+        :incomplete => 0,
+        :complete => 1
+    }
+
+    def complete?
+        self.status == STATUS[:complete]
+    end 
+
+    def incomplete?
+        self.status == STATUS[:incomplete]
+    end
+
 end
