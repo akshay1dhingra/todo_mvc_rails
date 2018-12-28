088ec9cdb8d4a9ee08a78946409c6b96b06d9153
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-28 15:49:48 -0600

    built out update method for check-boxes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,11 +12,11 @@ class ItemsController < ApplicationController
 
   # PATCH - /lists/:list_id/items/:id
   def update
-    raise params.inspect
-    # @item = Item.find(params[:id])
-    # @item.update(item_params)
+    # raise params.inspect
+    @item = Item.find(params[:id])
+    @item.update(item_params)
 
-    # redirect_to list_path(@item.list)
+    redirect_to list_path(@item.list)
   end
 
   private
