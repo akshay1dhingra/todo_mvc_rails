190d4d21b3fe27fd1c224f086be780ad40361b10
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-02 13:38:27 -0600

    made destroy method for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,12 @@ class ItemsController < ApplicationController
     redirect_to list_path(@item.list)
   end
 
+  def destroy 
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to list_path(@item.list)
+  end 
+
   private
     def item_params
       params.require(:item).permit(:description, :status)
