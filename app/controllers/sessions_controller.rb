2f988dd09c057b4fe3617a4545d85f1d9ef95112
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 17:28:36 -0600

    added methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+    
+    def new 
+    end 
+
+    def create
+        # raise params.inspect
+        user = User.find_by(:email => params[:email])
+        session[:user_id] = user.id 
+        redirect_to root_path
+    end 
+
+    def destroy 
+        reset_session
+        redirect_to root_path
+    end 
+
 end
