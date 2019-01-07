445e5a37640d1d203a54574871ec876f69373d9f
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 15:26:31 -0600

    created user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_162824) do
+ActiveRecord::Schema.define(version: 2019_01_07_211809) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_12_28_162824) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
