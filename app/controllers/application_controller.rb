58e590386f73c6c60e38cb221914e604c93b0255
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 17:28:03 -0600

    added authentication and logged in methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def authentication_required 
+        if !logged_in?
+            redirect_to login_path
+        end 
+    end 
+
+    def logged_in?
+        session[:user_id]
+    end
+
 end
