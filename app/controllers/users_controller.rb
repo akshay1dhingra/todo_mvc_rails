8a5750defac0a3d274984fe5fa6bf60d64b55f99
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 16:52:39 -0600

    created user email logic for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
     end 
 
     def create 
-
+        # raise params.inspect
+        @user = User.new
+        @user.email = params[:user][:email]
+        if @user.save
+            redirect_to lists_path
+        else 
+            render '/users/new' 
+        end
     end 
 
 end
