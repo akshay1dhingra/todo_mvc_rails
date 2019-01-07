dbf3380f77e164a5923b0da2b7a444da9b6a9752
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 17:28:55 -0600

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
+
+  get '/logout' => "sessions#destroy"
   
   resources :users, :only => [:new, :create]
 
