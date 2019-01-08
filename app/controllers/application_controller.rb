c034dee412758b4797a3d97cde7191eb22999744
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 22:31:58 -0600

    added current_user method to controller and used in index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,13 @@ class ApplicationController < ActionController::Base
     end 
 
     def logged_in?
-        session[:user_id]
+        !!current_user
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    helper_method :current_user
 
 end
