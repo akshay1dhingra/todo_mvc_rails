74bd61debb063515756c3e575ba12ebe87cb7772
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-07 23:11:35 -0600

    added password_digest for users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_211809) do
+ActiveRecord::Schema.define(version: 2019_01_08_043358) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_01_07_211809) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
